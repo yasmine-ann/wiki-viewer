040f68390fa1b404d0340731d5a6659b832141ff
Yasmine Triola <yasmine.triola@gmail.com>
2017-05-03 17:13:43 -0400

    Add random button event listener and ajax request.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var model = {
 var controller = {
   init: function init() {
     controller.setupListener();
+    controller.setupRandomListener();
 
   },
 
@@ -26,12 +27,32 @@ var controller = {
     });
   },
 
+  getRandomArticle: function getRandomArticle() {
+    var url = "https://en.wikipedia.org/w/api.php?action=query&prop=info|extracts&exsentences=1&exintro&explaintext&exlimit=1&inprop=url&generator=random&grnnamespace=0&grnlimit=1&format=json"
+
+    $.ajax({
+      url: url,
+      dataType: 'jsonp'
+    })
+    .done(function(data) {
+      var randomWikiData = data;
+      console.log(randomWikiData);
+    });
+  },
+
   setupListener: function setupListener() {
     $("#search-button").click(function searchClick() {
       console.log("CLICK!");
       controller.getArticle();
 
     });
+  },
+
+  setupRandomListener: function setupRandomListener() {
+    $("#search-random").click(function randomClick() {
+      console.log("Random Click!");
+      controller.getRandomArticle();
+    })
   }
 
 }
